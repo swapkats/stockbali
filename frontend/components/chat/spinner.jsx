872690211b30ba0c9df270dabce007329f494e79
Swapnil Singh <mail@swapnilsingh.me>
2017-10-01 13:49:51 +0530

    Restyles loading screen

--- frontend/components/chat/spinner.jsx
+++ frontend/components/chat/spinner.jsx
@@ -5,8 +5,8 @@ const Spinner = () => {
   return (
     <div className='spinnerContainer'>
       <img className='spinnerLogo' src={ window.assets.bannerLogo }/>
-      <h1>Loading StockBaat...</h1>
-      <MDSpinner size={60} />
+      <h1>StockBaat</h1>
+      <MDSpinner size={20} />
     </div>
   );
 };
